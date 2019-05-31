20caa9d8b125e4269f2438980fd2e592e92a54e1
megan schulte <megan.schulte.pdx@gmail.com>
2019-05-31 11:30:42 -0700

    make changes to age calc function

--- src/age-calc.js
+++ src/age-calc.js
@@ -1,3 +1,10 @@
 export function AgeCalc(dob) {
-  let userAge = date.getDate() -
+  let currentDate = new Date();
+  let birthDate = new Date(dob);
+  let userAge = today.getFullYear() - birthDate.getFullYear();
+  let month = today.getMonth();
+    if(month < 0 || (month === 0 && today.getDate())) {
+      userAge = userAge - 1;
+    }
+    return userAge;
 }
