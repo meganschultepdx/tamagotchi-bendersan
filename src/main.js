befec6768486e641e96b86cdbff10c5dd573d0ee
megan schulte <megan.schulte.pdx@gmail.com>
2019-06-03 11:48:06 -0700

    add index.html with content

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { Tamagotchi } from './tamagotchi';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+// import img from './image.png'
+
+$(document).ready(function() {
+  $('#dob-form').submit(function(event) {
+    event.preventDefault();
+
+
+  });
+});
